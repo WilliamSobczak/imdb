ebbf371ac4dce9944eaffe063cb33e407845c296
WilliamSobczak <williamjsobczak@gmail.com>
2023-04-23 20:39:03 -0400

    added quicksort and tested on simple arrays

--- main.cpp
+++ main.cpp
@@ -2,29 +2,73 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 void mergeSort(int arr[], int left, int right);
 void merge(int arr[], int left, int mid, int right);
 
+void quickSort(int arr[], int low, int high);
+int partition(int array[], int low, int high)
+{
+    int pivot = array[low];
+    int up = low, down = high;
+
+    while (up < down) {
+        for (int j = up; j < high; j++) {
+            if (array[up] > pivot) {
+                break;
+            }
+            up ++;
+        }
+        for (int j = high; j > low; j--) {
+            if (array[down] < pivot) {
+                break;
+            }
+            down--;
+        }
+        if (up < down) {
+            swap(array[up], array[down]);
+        }
+    }
+    swap(array[low], array[down]);
+    return down;
+}
+
 
 
 int main() {
 
-    cout << "test" << endl;
-
+    //Testing Merge Sort and quick sort on simple arrays
     int arr1[10] = {9, 3, 4, 7, 10, 1, 2, 5, 6, 8};
+    int arr2[10] = {9, 3, 4, 7, 10, 1, 2, 5, 6, 8};
     int length = sizeof(arr1)/sizeof(arr1[0]);
     mergeSort(arr1, 0, length-1);
+    quickSort(arr2, 0, length-1);
 
     for (int i = 0; i < length; i++) {
         cout << arr1[i] << " ";
     }
+    cout << endl;
+
+    for (int i = 0; i < length; i++) {
+        cout << arr2[i] << " ";
+    }
+    //end testing
 
     return 0;
 }
 
 
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high)
+    {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
 
 void mergeSort(int arr[], int left, int right) {
     if (left < right) {
