d47cad545243a4fc97365757e1d8ecb814b9025e
WilliamSobczak <williamjsobczak@gmail.com>
2023-04-26 22:00:23 -0400

    quicksort fix

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,10 @@
-printMovies(movies);
+//quick sort
+    // auto quick_start_time = chrono::high_resolution_clock::now();
+
+    // if (ordering_choice ==1) {
+    //     quickSort(moviesFiltered, 0, moviesFiltered.size() - 1, true); 
+    // }
+    // else {
+    //     quickSort(moviesFiltered, 0, moviesFiltered.size() - 1, false); 
+    // }
+    // auto quick_end_time = chrono::high_resolution_clock::now();
